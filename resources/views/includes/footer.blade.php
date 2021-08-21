6ce4d9e2fcaabb67b2a2f0264f0b3c53b70ab1a5
riski maulana rahman <riskimaulanarahman@gmail.com>
2021-08-21 17:35:43 +0800

    add footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- begin #footer -->
 <div id="footer" class="footer">
-	&copy; 2021 SIMFEL | Sistem Informasi Management File - <a href="https://pinday.co.id" target="_blank">Pinday</a> All Rights Reserved
+	&copy; 2021 FIF Group | Surveyor - <a href="#" target="_blank">Kukuh</a> All Rights Reserved
 </div>
 <!-- end #footer -->
